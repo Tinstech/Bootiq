44f751b96c8e79ff7a30d708c0205671a3cfea2f
František <frantisek.kryzl@bootiq.io>
2020-05-05 16:40:13 +0200

    test pre pridanie uzivatela poskytovatela

---
+++ cypress/integration/DM/newDiscountProviderUser.ts
@@ -0,0 +1,29 @@
+import {
+  LogInDMPage,
+  ProvidersAccountsPage,
+  ProvidersPage,
+} from '../../support/elements/DM';
+import routes from '../../support/testRoutes';
+
+describe('Create new discount provider', () => {
+  beforeEach('preserve cookies', () => {
+    Cypress.Cookies.preserveOnce('PHPSESSID');
+  });
+  after('clear cookies', () => {
+    cy.clearCookies();
+  });
+
+  it('log in as admin', () => {
+    cy.visit(`${Cypress.env('DM_BASE_URL')}${routes.adminDMLoginPage}`);
+    LogInDMPage().logInDMAdmin();
+  });
+  it('create new provider then create new branch', () => {
+    ProvidersPage().newDiscountProvider();
+    cy.uploadFile(
+      'snoop_dogg.jpg',
+      '#provider_logo-fileInput',
+      'image/jpg',
+    ).uploadFile('snoop_dogg.jpg', '#provider_image-fileInput', 'image/jpg');
+    ProvidersAccountsPage().newProviderUser();
+  });
+});
--- cypress/support/elements/DM/index.ts
+++ cypress/support/elements/DM/index.ts
@@ -1,4 +1,5 @@
 export { BranchesPage } from './BranchesPage';
 export { DiscountsPage } from './DiscountsPage';
 export { LogInDMPage } from './LogInDMPage';
+export { ProvidersAccountsPage } from './ProvidersAccountsPage';
 export { ProvidersPage } from './ProvidersPage';
